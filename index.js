4dfa845f70814aa180ecaa996640c5e88d931f43
javoh1rqaxramonov <ubotirov22@.com>
2024-07-27 15:43:05 +0500

    finish

---
+++ index.js
@@ -0,0 +1,147 @@
+// 1-masala 
+// let a = 10;
+// let arr=[];
+// for(let i=1;i<=a;i++){
+//     arr.push(i);
+// }
+// console.log(arr);
+
+// 2-masala 
+// let arr = [1,2,4,5,67,8,9];
+// let newarr1=[];
+// let newarr2=[];
+// for(let i=0;i<arr.length;i++){
+//     if (arr[i]%2==0) {
+//         newarr1.push(arr[i]);
+//     } else {
+//         newarr2.push(arr[i]);
+//     }
+// }
+// console.log(newarr1);
+// console.log(newarr2);
+
+// 3-masala 
+// let arr = [1,2,34,5,6];
+// let arr1 = [12,4,56,67,4];
+// let sum=0;
+// let sum1=0;
+// let res =0;
+// for(let i=0;i<arr.length;i++){
+//     sum+=arr[i];
+// }
+// for(let i=0;i<arr.length;i++){
+//     sum1+=arr1[i];
+// }
+// res=sum+sum1;
+// console.log(res);
+
+// 4-masala 
+// let arr=[1,2,3,4,67,8];
+// let newarr=[];
+// for(let i=arr.length-1; i>=0;i--){
+//     newarr.push(arr[i]);
+
+// }console.log(newarr);
+
+// 5-masala 
+
+// 1-masala 
+// let arr = [1,2,43,6,7,9];
+// let newarr= [];
+// for(let i=0;i<arr.length;i++){
+//     newarr.push(arr[i]);
+// }
+// console.log(newarr .sort(a-b));
+
+// 2-masala 
+// let arr = ["cvbn","dfgh",1,2,343];
+// let newarr= [];
+// for (const iterator of arr) {
+//     if (typeof iterator=="string") {
+//         newarr.push(iterator)
+//     }
+// }
+// console.log(newarr);
+
+
+// 3-masala 
+// let arr=[1,2,3];
+// arr.splice(1,1,3);
+// console.log(arr);
+
+
+// 4-masala 
+// let arr = [2,99,6,5,10];
+//  let res= [];
+// for(let  i = arr.length-1; i>= 0 ; i--){
+//     if(arr[i] % 2==1){
+//       arr[i] = 0
+//     }
+//    res.push(arr[i]); 
+
+// }
+// console.log(res);
+
+// 5-masala 
+// let arr = [3,5,7,8];
+// arr.splice(0,1,0);
+// arr.splice(1,1,1);
+// arr.splice(2,1,2);
+// arr.splice(3,1,3)
+// console.log(arr);
+
+// 6-masala 
+// let arr=[3,4,6,8,9,12];
+// let newarr=[];
+// for (const iterator of arr) {
+//     if (iterator%3==0) {
+//         newarr.push(iterator)
+//     }
+// }
+// console.log(newarr);
+
+// 9-masala 
+// let arr = [1,2,3,-4,-7];
+// let newarr=[];
+// for(let i=0;i<arr.length;i++){
+//     if (arr[i]>0) {
+//        newarr.push(arr[i]); 
+//     }
+// }
+// console.log(newarr.length);
+
+// 10-masala 
+// let arr = [1,2,3,-4,-7];
+// let newarr=[];
+// let res=0;
+// for(let i=0;i<arr.length;i++){
+//     if (arr[i]>0) {
+//        newarr.push(arr[i]); 
+//        res+=arr[i]; 
+//     }
+// }
+// console.log(res);
+
+// 11-masala 
+// let arr= ["hi","salom"];
+// let newarr=[];
+
+// for(let i=0;i<arr.length;i++){
+//     newarr.push(arr[i].at(0))
+// }
+
+// console.log(newarr);
+
+// 12-masala 
+// let arr=[1,2,4,5,7,-9,-4];
+// let newarr=[];
+// let sum=1;
+// for(let i=0;i<arr.length;i++){
+//     if (arr[i]>0) {
+//         newarr.push(arr[i]);
+//         sum*=newarr[i];
+//     }
+// }
+// console.log(sum);
+
+// 13-masala 
